0e83a4a1b8b67777c92d3f1151274a75357390ec
Joshua Gauthreaux <joshuagauthreaux@gmail.com>
2020-02-18 04:39:45 +0000

    Automatically backed up by Learn

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -1,5 +1,5 @@
 require 'pry'
-
+# binding.pry
 # Build a nested HoH of the classification of biological life on Earth based
 # on these commented-out snippets assigned to PORTION_1-PORTION_4. Return
 # that HoH as the return value for the method "naming_system."
@@ -59,7 +59,7 @@ def naming_system
             sub_category: {
               label: "Species",
               sub_category: {}
-              binding.
+              binding.pry
             }
           }
         }
